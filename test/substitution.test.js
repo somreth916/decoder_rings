0816641f240816fce61863a80cff1696ae6d1a43
Somreth Peng <57914599+somreth916@users.noreply.github.com>
2021-03-19 12:46:58 -0700

    initial commit

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,6 +1,46 @@
 // Write your tests here!
 const expect = require("chai").expect;
-const substitutionModule = require("../src/substitution");
+const substitution = require("../src/substitution");
+
+describe("substitution() tests continued", () => {
+    it("Returns false if given alphabet isn't 26 characters long.", () => {
+        const input = "hello";
+        const alphabet = "abcdefg";
+        let actual = substitution(input, alphabet, encode = true);
+        expect(actual).to.eql(false);
+    });
+
+    it("Sucessfully translates the phrase.", () => {
+        const input = "hello";
+        const alphabet = "zyxwvutsrqponmlkjihgfedcba";
+        const expected = "svool";
+        let actual = substitution(input, alphabet, encode = false);
+        expect(actual).to.eql(expected);
+    });
+
+    it("Returns false if there are any duplicates in the alphabet.", () => {
+        const input = "thinkful";
+        const alphabet = "abccdefghijklmnopqrstuvwxy";
+        let actual = substitution(input, alphabet, encode = true);
+        expect(actual).to.eql(false);
+    });
+
+    it("Maintains spacing in message along with special characters.", () => {
+        const input = "Space Space";
+        const alphabet = "zyxwvutsrqponmlkjihgfedcba";
+        const expected = "hkzxv hkzxv";
+        let actual = substitution(input, alphabet, encode = true);
+        expect(actual).to.eql(expected);
+    });
+
+    it("Should ignore capital letters.", () => {
+        const input = "Space";
+        const alphabet = "zyxwvutsrqponmlkjihgfedcba";
+        const expected = "hkzxv";
+        let actual = substitution(input, alphabet, encode = true);
+        expect(actual).to.eql(expected);
+    });
+})
 
 // It returns false if the given alphabet isn't exactly 26 characters long.
 // It correctly translates the given phrase, based on the alphabet given to the function.
