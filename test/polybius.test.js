8444a3fe6ff4c989fd4b42b6ff9ff9b173ebc37c
Somreth Peng <57914599+somreth916@users.noreply.github.com>
2021-03-19 12:46:33 -0700

    initial commit

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,6 +1,36 @@
 // Write your tests here!
 const expect = require("chai").expect;
-const polybiusModule = require("../src/polybius");
+const polybius = require("../src/polybius");
+
+describe("polybius() tests continued", () => {
+    it("When encoding, translate letters i and j to 42.", () => {
+        const input = "ij";
+        const expected = "4242";
+        let actual = polybius(input, encode = true);
+        expect(actual).to.eql(expected);
+    });
+
+    it("When decoding, translate 42 into (i/j).", () => {
+        const input = "42";
+        const expected = "(i/j)";
+        let actual = polybius(input, encode = false);
+        expect(actual).to.eql(expected);
+    });
+
+    it("Should ignore capital letters.", () => {
+        const input = "Space";
+        const expected = "3453113151";
+        let actual = polybius(input, encode = true);
+        expect(actual).to.eql(expected);
+    });
+
+    it("Maintains spacing in message along with special characters.", () => {
+        const input = "Space Space";
+        const expected = "3453113151 3453113151";
+        let actual = polybius(input, encode = true);
+        expect(actual).to.eql(expected);
+    });
+})
 
 // When encoding, it translates the letters i and j to 42.
 // When decoding, it translates 42 to (i/j).
